366a8bc3f06f45adfc3c041644b5d0a25cbc547c
Olli Machina <olli.machina@mymail.champlain.edu>
2021-10-31 12:10:22 -0400

    Plants rotating to face away from the wall

--- PlantMan/Assets/Scripts/Plants/WallStick.cs
+++ PlantMan/Assets/Scripts/Plants/WallStick.cs
@@ -23,12 +23,14 @@ public class WallStick : MonoBehaviour
     {
         if(collision.gameObject.tag == "Wall") //Make this object stick to a wall
         {
+            Debug.Log("wall" + collision.transform.right);
             Rigidbody rb = GetComponent<Rigidbody>();
             rb.velocity = Vector3.zero;
             rb.useGravity = false;
             rb.mass = 999999999; //If the mass is relatively huge, it will not be able to be moved by forces.
-            Vector3 vel = collision.contacts[0].normal * -90; //Makes the seed face the normal vector to the wall.
-            rb.rotation = Quaternion.Euler(vel.y, vel.z, vel.x);//Makes the seed face the normal vector to the wall.
+            transform.rotation = Quaternion.Euler(90f * collision.transform.right);
+            //Vector3 vel = collision.contacts[0].normal * -90; //Makes the seed face the normal vector to the wall.
+            //rb.rotation = Quaternion.Euler(vel.y, vel.z, vel.x);//Makes the seed face the normal vector to the wall.
         }
     }
 }
