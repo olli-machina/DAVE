c861ec961f8c9f253550301465264479678f30c7
Carter Ivancic <carter.ivancic@mymail.champlain.edu>
2021-10-31 20:47:22 -0400

    Creating a prefab for the tutorial UI. Creating a script for the tutorial UI. Creating a collider prefab for updating the tutorial UI

---
+++ PlantMan/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialScript : MonoBehaviour
+{    
+    public Canvas tutorialCanvas;
+    public TextMeshProUGUI title, description;
+    public string titleText, descrText;
+    GameObject gamemanager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamemanager = GameObject.Find("GameManager");
+    }
+
+    /*
+    * Purpose: Updates the tutorial UI for the corresponding collider in the world, then pauses the game
+    * References: None
+    * Scripts Called: None
+    * Status: 
+    * Contributor(s): Carter Ivancic
+    */
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            gamemanager.GetComponent<GameManager>().gameUI.gameObject.SetActive(false);
+            tutorialCanvas.gameObject.SetActive(true);
+            Time.timeScale = 0.0f;
+            title.text = titleText;
+            description.text = descrText;
+        }
+        
+    }
+
+    /*
+    * Purpose: Closes the tutorial UI, destroys the collider and resumes the game
+    * References: called by InputManager attached to player object
+    * Scripts Called: None
+    * Status: 
+    * Contributor(s): Carter Ivancic
+    */
+    public void CloseTutorial(InputAction.CallbackContext context)
+    {
+        if (Time.timeScale == 0.0f)
+        {
+            tutorialCanvas.gameObject.SetActive(false);
+            gamemanager.GetComponent<GameManager>().gameUI.gameObject.SetActive(true);
+            Time.timeScale = 1.0f;
+            Destroy(this.gameObject);
+        }
+    }
+}
