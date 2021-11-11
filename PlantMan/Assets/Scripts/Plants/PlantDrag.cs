3e84bbcd1b111f71cf1b34fdc36fd90839626107
Olli Machina <olli.machina@mymail.champlain.edu>
2021-11-10 22:15:33 -0500

    Seeds don't slide on the ground

---
+++ PlantMan/Assets/Scripts/Plants/PlantDrag.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantDrag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Ground")
+            Debug.Log(other.gameObject.name + GetComponent<Collider>().GetType());
+        if(GetComponent<Collider>().GetType() == typeof(SphereCollider) && other.gameObject.layer == 8)
+        {
+            gameObject.GetComponent<Rigidbody>().drag = 30f;
+        }
+    }
+}
