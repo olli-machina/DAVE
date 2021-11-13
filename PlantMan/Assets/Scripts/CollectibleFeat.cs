c69c6f3398f5c64995c4f7ab38d59ae3fd57bd5c
Brandon L'Abbe <brandon.labbe@mymail.champlain.edu>
2021-11-13 13:55:30 -0500

    Collectible Feat Implementation

---
+++ PlantMan/Assets/Scripts/CollectibleFeat.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleFeat : MonoBehaviour
+{
+
+    private int numOfCollectibles;
+
+    private bool[] collectiblesCollected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numOfCollectibles = GameObject.FindGameObjectsWithTag("Collectible").Length;
+        collectiblesCollected = new bool[numOfCollectibles];   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Collect(CollectibleData data)
+    {
+        int ID = data.ID;
+
+        collectiblesCollected[ID] = true;
+        
+        for(int i = 0; i< numOfCollectibles; i++)
+        {
+            if (!collectiblesCollected[ID])
+                return;
+        }
+
+        //Will only get here if all achievements have been collected
+        GameObject.Find("FeatManager").GetComponent<FeatManager>().fireFeat("Collectables");
+    }
+
+    public bool getCollectableState(int ID)
+    {
+        return collectiblesCollected[ID];
+    }
+}
